47d671d8fe6d74ccb53ef41e2f90ca1eff37607b
Kacper Lagowski <kacper.lagowski@senecaim.com>
2017-11-08 17:13:32 +0000

    Arrival price db insert completed

--- PriceUpdate/PriceUpdate/ArrivalPrice.cs
+++ PriceUpdate/PriceUpdate/ArrivalPrice.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,19 +21,46 @@ namespace PriceUpdate
         public string ID_DataFeed { get; set; }
         public decimal Price { get; set; }
         public DateTime PriceDateTime { get; set; }
-        public IntradayPrice IntradayFlag { get; set; }
+        public IntradayPrice PriceFlag { get; set; }
 
 
         public ArrivalPrice(DataRow dr)
         {
             this.ID = Convert.ToInt32(dr["ID"]);
             this.DealID = Convert.ToInt32(dr["DealID"]);
+            this.ID_DataFeed = dr["ID_DataFeed"].ToString();
             this.PriceDateTime = Convert.ToDateTime(dr["PriceDateTime"]);
+            this.Price = createPrice();
         }
 
-        public void CreatePrice()
+        private decimal createPrice()
         {
+            this.PriceFlag = IntradayPrice.Opening;
             
+        }
+        public void Update(SqlConnection conn)
+        {
+            conn.Open();
+            string _storedProcedure = "sp_PMInstrument_InsertArrivalPrice";
+            try
+            {
+                SqlCommand _cmd = new SqlCommand(_storedProcedure, conn);
+                #region DB Parameters
+                _cmd.Parameters.Add("@ID_MPM", SqlDbType.Int).Value = InstrumentID;
+                _cmd.Parameters.Add("@ID_Date", SqlDbType.DateTime).Value = PriceDateTime;
+                _cmd.Parameters.Add("@Data_Price", SqlDbType.Decimal).Value = Price;
+                _cmd.Parameters.Add("@Data_Price", SqlDbType.Int).Value = PriceFlag;
+                #endregion
+                _cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                System.Windows.Forms.MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
